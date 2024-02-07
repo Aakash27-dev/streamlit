c88560d886424d5f3204b0f2d23447f4cbf99cc5
Aakash27-dev <159119487+Aakash27-dev@users.noreply.github.com>
2024-02-07 15:30:40 +0545

    Update app.py

--- app.py
+++ app.py
@@ -12,35 +12,35 @@ def process_image(input_image):
 
 def main():
 # Create input and output directories if they don't exist
-input_dir = "input"
-output_dir = "output"
-os.makedirs(input_dir, exist_ok=True)
-os.makedirs(output_dir, exist_ok=True)
+    input_dir = "input"
+    output_dir = "output"
+    os.makedirs(input_dir, exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
 
-st.title("Jigsaw puzzle Solver")
+    st.title("Jigsaw puzzle Solver")
 
 # File uploader for input image
-uploaded_file = st.file_uploader("Choose an input image...", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("Choose an input image...", type=["jpg", "jpeg", "png"])
 
-if uploaded_file is not None:
+    if uploaded_file is not None:
     # Display the uploaded input image
-    st.text("Input Image")
-    input_image = Image.open(uploaded_file)
-    st.image(input_image, caption="Uploaded Input Image", use_column_width=True)
+       st.text("Input Image")
+       input_image = Image.open(uploaded_file)
+       st.image(input_image, caption="Uploaded Input Image", use_column_width=True)
 
     # Save the input image to the input folder
-    input_filename = os.path.join(input_dir, f"input_{len(os.listdir(input_dir)) + 1}.jpeg")
-    input_image.save(input_filename)
+       input_filename = os.path.join(input_dir, f"input_{len(os.listdir(input_dir)) + 1}.jpeg")
+       input_image.save(input_filename)
 
     # Button to process the image
-    if st.button("Process Image"):
-        output_image = process_image(input_image)
-        # Display the transformed output image
-        st.image(output_image, caption="Transformed Output Image", use_column_width=True)
+       if st.button("Process Image"):
+          output_image = process_image(input_image)
+          # Display the transformed output image
+          st.image(output_image, caption="Processed Output Image", use_column_width=True)
 
-        # Save the output image to the output folder
-        output_filename = os.path.join(output_dir, f"output_{len(os.listdir(output_dir)) + 1}.png")
-        output_image.save(output_filename)
+          # Save the output image to the output folder
+          output_filename = os.path.join(output_dir, f"output_{len(os.listdir(output_dir)) + 1}.png")
+          output_image.save(output_filename)
 
 if __name__ == "__main__":
     main()
